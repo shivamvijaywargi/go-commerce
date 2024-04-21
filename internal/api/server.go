65981356f335190ad18c843590057d2e87142a1a
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-04-21 17:28:12 +0530

    feat: add health check route

--- internal/api/server.go
+++ internal/api/server.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/shivamvijaywargi/go-commerce/configs"
 )
@@ -8,5 +10,14 @@ import (
 func StartServer(config configs.AppConfig) {
 	app := fiber.New()
 
+	app.Get("/health-check", HealthCheck)
+
 	app.Listen(config.ServerPort)
+}
+
+func HealthCheck(ctx *fiber.Ctx) error {
+	return ctx.Status(http.StatusOK).JSON(fiber.Map{
+		"status":  "ok",
+		"message": "Server is up and running",
+	})
 }
